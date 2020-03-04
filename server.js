0d412393b310152ccf27e259ad0d611ac8ef8c5e
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-03-04 13:43:42 +0000

    theory and mockup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.use
 app.get("/",(req,res)=>{
     return res.send("Hello world")
 })
@@ -11,3 +12,6 @@ app.get("/",(req,res)=>{
 app.listen(PORT,(req,res)=>{
     console.log(`server is working on port ${PORT}`)
 })
+
+//https://developer.edamam.com/admin/applications/1409619312049
+//https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
