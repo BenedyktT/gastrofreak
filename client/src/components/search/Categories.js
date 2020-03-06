b134f6ab43ae1ae3454069200ebf6a30cb3f5837
BenedyktT <benedykt.tyminski@gmail.com>
2020-03-06 23:47:01 +0000

    working at searching component

---
+++ client/src/components/search/Categories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <div className="category">
+      <h3 className="text-big">Or pick category:</h3>
+      <div className="category__grid"></div>
+    </div>
+  );
+};
+
+export default Categories;
