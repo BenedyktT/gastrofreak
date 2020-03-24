18b47b1af52efb8073536e607f96586f778b98e7
BenedyktT <benedykt.tyminski@gmail.com>
2020-03-24 22:25:08 +0000

    refactor folder structure to be separate to resolve issue with node_modules hoisting and jest

--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,21 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      process.env.MONGO_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-      },
-      console.log("connected to db")
-    );
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
-  }
+	try {
+		await mongoose.connect(
+			process.env.MONGO_URI,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+				useCreateIndex: true,
+				useFindAndModify: false
+			},
+			console.log("connected to db")
+		);
+	} catch (error) {
+		console.error(error.message);
+		process.exit(1);
+	}
 };
 
-export default connectDB;
+module.exports = connectDB;
