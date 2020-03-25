68dbcabe9b77ddc2f32dbd75367b9f83a64d718e
BenedyktT <benedykt.tyminski@gmail.com>
2020-03-25 22:49:36 +0000

    getMeal test

--- api/helper/getMeal.js
+++ api/helper/getMeal.js
@@ -1,40 +1,60 @@
 const axios = require("axios");
-
-module.exports = async (queryType, item, serving = 1) => {
-	try {
-		const type = queryType === "s" ? "search" : "lookup";
-		const response = await axios.get(
-			`https://www.themealdb.com/api/json/v1/1/${type}.php?${queryType}=${item}`
-		);
-
-		if (response.data.meals) {
-			const recipe = response.data.meals.map((meal, i, arr) => ({
-				title: meal.strMeal,
-				prep: meal.strInstructions,
-				ingr: arr.reduce((acc, curr) => {
-					const extractItem = includes => {
-						return (
-							Object.keys(meal)
-								//get keys where there are ingredients
-								.filter(e => e.includes(includes))
-								//create object with ingredients only
-								.map(key => curr[key] && curr[key])
-								//remove null and empty strings
-								.filter(e => e)
-						);
-					};
-					const ingr = extractItem("strIngredient");
-					const measures = extractItem("strMeasure");
-					//combine measure and ingredients
-					acc = ingr.map((e, i) => `${measures[i]} ${e}`);
-					return acc;
-				}, [])
-			}));
-			return recipe;
-		}
-		return [{ msg: "Wrong ID - this recipe does not exist" }];
-	} catch (error) {
-		console.error(error);
-		return error;
-	}
+const fetchRecipe = async (queryType, item) => {
+  try {
+    const type = queryType === "s" ? "search" : "lookup";
+    const response = await axios.get(
+      `https://www.themealdb.com/api/json/v1/1/${type}.php?${queryType}=${item}`
+    );
+    return response.data.meals;
+  } catch (error) {
+    throw new Error("Error");
+  }
 };
+
+const trimRecipe = meals => {
+  if (!meals) {
+    return "Invalid argument";
+  }
+  const recipe = meals.map((meal, i, arr) => ({
+    title: meal.strMeal,
+    prep: meal.strInstructions,
+    ingr: arr.reduce((acc, curr) => {
+      const extractItem = includes => {
+        return (
+          Object.keys(meal)
+            //get keys where there are ingredients
+            .filter(e => e.includes(includes))
+            //create object with ingredients only
+            .map(key => curr[key] && curr[key])
+            //remove null and empty strings
+            .filter(e => e)
+        );
+      };
+      const ingr = extractItem("strIngredient");
+      const measures = extractItem("strMeasure");
+      //combine measure and ingredients
+      acc = ingr.map((e, i) => `${measures[i]} ${e}`);
+      return acc;
+    }, [])
+  }));
+  return recipe;
+};
+
+const getMeal = async (queryType, item, serving = 1) => {
+  try {
+    if (!item) {
+      return [{ msg: "Wrong ID - this recipe does not exist" }];
+    }
+    const recipe = await fetchRecipe(queryType, item).catch(e =>
+      console.error(e)
+    );
+
+    return trimRecipe(recipe);
+  } catch (error) {
+    console.log(error);
+    throw new Error("error");
+  }
+};
+exports.getMeal = getMeal;
+exports.fetchRecipe = fetchRecipe;
+exports.trimRecipe = trimRecipe;
--- api/helper/getNutritionValues.test.js
+++ api/helper/getNutritionValues.test.js
@@ -1,37 +1,37 @@
 const axios = require("axios");
 const getNutritionValues = require("./getNutritionValues");
 const exampleDataToConvert = {
-	calories: 1200,
-	totalWeight: 1200,
-	dietLabels: ["array"],
-	healthLabels: ["array"],
-	cautions: ["array"],
-	totalNutrients: ["array"],
-	totalDaily: ["array"]
+  calories: 1200,
+  totalWeight: 1200,
+  dietLabels: ["array"],
+  healthLabels: ["array"],
+  cautions: ["array"],
+  totalNutrients: ["array"],
+  totalDaily: ["array"]
 };
 jest.mock("axios");
 
 describe("getNutritionValues", () => {
-	it("fetch data successfully", async () => {
-		axios.post.mockImplementationOnce(() =>
-			Promise.resolve({ data: exampleDataToConvert })
-		);
-		await expect(
-			getNutritionValues([{ prep: "prep", title: "title", ingr: ["ingr"] }])
-		).resolves.toHaveProperty(
-			"ingr",
-			"nutrients",
-			"prep",
-			"title",
-			"totalWeight"
-		);
-	});
-	it("fetch data with error", async () => {
-		expect.assertions(1);
-		/* axios.post.mockImplementationOnce(() =>
-			Promise.reject(new Error("Wrong Data Type"))
-		); */
+  it("fetch data successfully", async () => {
+    axios.post.mockImplementationOnce(() =>
+      Promise.resolve({ data: exampleDataToConvert })
+    );
+    await expect(
+      getNutritionValues([{ prep: "prep", title: "title", ingr: ["ingr"] }])
+    ).resolves.toHaveProperty(
+      "ingr",
+      "nutrients",
+      "prep",
+      "title",
+      "totalWeight"
+    );
+  });
+  it("fetch data with error", async () => {
+    expect.assertions(1);
+    axios.post.mockImplementationOnce(() =>
+      Promise.reject(new Error("Wrong Data Type"))
+    );
 
-		await expect(getNutritionValues([])).rejects.toThrow("Wrong data type");
-	});
+    await expect(getNutritionValues([])).rejects.toThrow("Wrong data type");
+  });
 });
